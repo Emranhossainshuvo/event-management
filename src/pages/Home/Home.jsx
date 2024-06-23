44b3882efdf0a75fbfef8ff82b78f6b596c71d4a
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-23 08:31:04 +0600

    set up the routes system as per react-router suggestions

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h3>Hello, this is the home page for the website</h3>            
+        </div>
+    );
+};
+
+export default Home;
